fbf7bd1a735f03ce563fa3f951073a193c699262
MGatev <marto032003@gmail.com>
2023-01-08 14:24:58 +0200

    Fixed functions declaration in header file

--- Minesweeper/Functions.h
+++ Minesweeper/Functions.h
@@ -4,7 +4,9 @@
 
 void InputValidation(int& boardSize, int& amountOfMines);
 
-void CreateBoardBeforeStart(char myBoard[][MAX_SIZE], const int boardSize);
+void CreateBoardBeforeStart(char myBoard[][MAX_SIZE], char playingBoard[][MAX_SIZE], const int boardSize);
+
+void placeMines(int mines[][2], const int amountOfMines, char realBoard[][MAX_SIZE], const int boardSize);
 
 void printBoard(char myBoard[][MAX_SIZE], const int boardSize);
 
