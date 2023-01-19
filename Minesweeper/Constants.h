5733da984141bc5a1dda88fcbf36636fe41be5fd
MGatev <marto032003@gmail.com>
2023-01-19 23:56:46 +0200

    Added all constants, mark cell feature

--- Minesweeper/Constants.h
+++ Minesweeper/Constants.h
@@ -1,5 +1,20 @@
+/**
+*
+* Solution to course project # 7
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Martin Gatev
+* @idnumber 0MI0600143* @compiler Visual Studio
+*
+* <File with constants>
+*
+*/
+
 #pragma once
 
 const int MAX_SIZE = 10;
 const int MAX_MINES = 30;
 const int MAX_MOVES = 70; //MAX_MOVES = 10 * 10 - 30
+const int MAX_LENGTH_COMMAND = 6;
--- Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper.cpp
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project # 7
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Martin Gatev
+* @idnumber 0MI0600143* @compiler Visual Studio
+*
+* <Main cpp file>
+*
+*/
+
+
 #include<iostream>
 #include"Functions.h"
 
@@ -36,7 +51,8 @@ void InputValidation(int& boardSize, int& amountOfMines)
 	}
 }
 
-void CreateBoardBeforeStart(char myBoard[][MAX_SIZE], char playingBoard[][MAX_SIZE], const int boardSize)
+void CreateBoardBeforeStart(char myBoard[][MAX_SIZE], char playingBoard[][MAX_SIZE],
+	const int boardSize)
 {
 	// Assign all the cells as mine-free
 	for (int i = 0; i < boardSize; i++)
@@ -50,7 +66,8 @@ void CreateBoardBeforeStart(char myBoard[][MAX_SIZE], char playingBoard[][MAX_SI
 	return;
 }
 
-void placeMines(int mines[][2], const int amountOfMines, char playingBoard[][MAX_SIZE], const int boardSize)
+void placeMines(int mines[][2], const int amountOfMines, char playingBoard[][MAX_SIZE],
+	const int boardSize)
 {
 	//mines store the coordinates of where a mine is placed
 
@@ -91,7 +108,7 @@ void printBoard(char myBoard[][MAX_SIZE], const int boardSize)
 
 	for (int i = 0; i < boardSize; i++)
 	{
-		printf("%d ", i);
+		printf("%d    ", i);
 	}
 
 	printf("\n\n");
@@ -102,10 +119,13 @@ void printBoard(char myBoard[][MAX_SIZE], const int boardSize)
 
 		for (int j = 0; j < boardSize; j++)
 		{
-			printf("%c ", myBoard[i][j]);
+			printf("%c    ", myBoard[i][j]);
 		}
 
 		printf("\n");
+		printf("\n");
+		printf("\n");
+
 	}
 }
 
@@ -217,126 +237,241 @@ int countAdjacentMines(int row, int col, int mines[][2],
 	return counter;
 }
 
-void makeMove(int* x, int* y)
+char* makeMove(int& x, int& y)//, char* &command)
 {
 	// Take the input move
-	printf("Enter your move, (row column): ");
+	printf("Enter your move, (command row column): ");
 
-	scanf_s("%d %d", x, y);
-	return;
+	char* command = new char[MAX_LENGTH_COMMAND];
+
+	cin >> command >> x >> y;
+
+	return command;
 }
 
 // A Recursive Function to play the Minesweeper Game
 bool playMinesweeperUtil(char myBoard[][MAX_SIZE], char playingBoard[][MAX_SIZE],
 						 int mines[][2], int amountOfMines,
-						 int boardSize, int row, int col, int* movesLeft)
+						 int boardSize, int row, int col, int* movesLeft, char* command)
 {
-	// Base Case
-	if (myBoard[row][col] != '-')
-		return (false);
+	bool isMark{}, isUnmark{}, isOpen{};
 
-	// You opened a mine
-	// Game Over
-	if (playingBoard[row][col] == '*')
+	if (command[0] == 'm' && command[1] == 'a' &&
+		command[2] == 'r' && command[3] == 'k')
 	{
-		myBoard[row][col] = '*';
-
-		for (int i = 0; i < amountOfMines; i++)
-		{
-			myBoard[mines[i][0]][mines[i][1]] = '*';
-		}
-
-		printBoard(myBoard, boardSize);
-
-		printf("\nYou lost!\n");
-
-		return (true);
+		isMark = true;
 	}
 
+	else if (command[0] == 'o' && command[1] == 'p' &&
+			 command[2] == 'e' && command[3] == 'n')
+	{
+		isOpen = true;
+	}
 	else
 	{
-		// Calculate the number of adjacent mines and put it
-		// on the board
-		int amountOfAdjacentMines = countAdjacentMines(row, col, mines, playingBoard, boardSize);
+		isUnmark = true;
+	}
 
-		(*movesLeft)--;
+	if (isMark == true)
+	{
+		myBoard[row][col] = '?';
+		return (false);
+	}
 
-		myBoard[row][col] = amountOfAdjacentMines + '0';
-
-		if (!amountOfAdjacentMines)
+	else if (isUnmark == true)
+	{
+		if (myBoard[row][col] == '?')
 		{
-			//Checking if the 'neighbour' cell is valid
-			//Checking if there is a mine there
-			//Checking the cells around the current cells
-			//clockwise starting from 12 o'clock 
-
-			//up 'neighbour' - 12 o'clock
-			if (isValid(row - 1, col, boardSize) == true)
-			{
-				if (isMine(row - 1, col, playingBoard) == false)
-					playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
-						boardSize, row - 1, col, movesLeft);
-			}
-
-			//diagonal up-right 'neighbour' - 1:30 o'clock
-			if (isValid(row - 1, col + 1, boardSize) == true)
-			{
-				if (isMine(row - 1, col + 1, playingBoard) == false)
-					playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
-						boardSize, row - 1, col + 1, movesLeft);
-			}
-
-			//right 'neighbour' - 3 o'clock
-			if (isValid(row, col + 1, boardSize) == true)
-			{
-				if (isMine(row, col + 1, playingBoard) == false)
-					playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
-						boardSize, row, col + 1, movesLeft);
-			}
-
-			//diagonal down-right 'neighbour' - 4:30 o'clock
-			if (isValid(row + 1, col + 1, boardSize) == true)
-			{
-				if (isMine(row + 1, col + 1, playingBoard) == false)
-					playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
-						boardSize, row + 1, col + 1, movesLeft);
-			}
-
-			//down 'neighbour' - 6 o'clock
-			if (isValid(row + 1, col, boardSize) == true)
-			{
-				if (isMine(row + 1, col, playingBoard) == false)
-					playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
-						boardSize, row + 1, col, movesLeft);
-			}
-
-			//diagonal down-left 'neighbour' - 7:30 o'clock
-			if (isValid(row + 1, col - 1, boardSize) == true)
-			{
-				if (isMine(row + 1, col - 1, playingBoard) == false)
-					playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
-						boardSize, row + 1, col - 1, movesLeft);
-			}
-
-			//left 'neighbour' - 9 o'clock
-			if (isValid(row, col - 1, boardSize) == true)
-			{
-				if (isMine(row, col - 1, playingBoard) == false)
-					playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
-						boardSize, row, col - 1, movesLeft);
-			}
-
-			//diagonal up-left 'neighbour' - 7:30 o'clock
-			if (isValid(row - 1, col - 1, boardSize) == true)
-			{
-				if (isMine(row - 1, col - 1, playingBoard) == false)
-					playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
-						boardSize, row - 1, col - 1, movesLeft);
-			}
+			myBoard[row][col] = '-';
 		}
 
 		return (false);
 	}
+
+	else if (isOpen == true)
+	{
+		// Base Case
+		if ((myBoard[row][col] != '-') && (myBoard[row][col] != '?'))
+			return (false);
+
+		// You opened a mine
+		// Game Over
+		if (playingBoard[row][col] == '*')
+		{
+			myBoard[row][col] = '*';
+
+			for (int i = 0; i < amountOfMines; i++)
+			{
+				myBoard[mines[i][0]][mines[i][1]] = '*';
+			}
+
+			printBoard(myBoard, boardSize);
+
+			printf("\nYou lost!\n");
+
+			return (true);
+		}
+
+		else
+		{
+			// Calculate the number of adjacent mines and put it
+			// on the board
+			int amountOfAdjacentMines = countAdjacentMines(row, col, mines, playingBoard, boardSize);
+
+			(*movesLeft)--;
+
+			myBoard[row][col] = amountOfAdjacentMines + '0';
+
+			if (!amountOfAdjacentMines)
+			{
+				//Checking if the 'neighbour' cell is valid
+				//Checking if there is a mine there
+				//Checking the cells around the current cells
+				//clockwise starting from 12 o'clock 
+
+				//up 'neighbour' - 12 o'clock
+				if (isValid(row - 1, col, boardSize) == true)
+				{
+					if (isMine(row - 1, col, playingBoard) == false)
+						playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
+							boardSize, row - 1, col, movesLeft, command);
+				}
+
+				//diagonal up-right 'neighbour' - 1:30 o'clock
+				if (isValid(row - 1, col + 1, boardSize) == true)
+				{
+					if (isMine(row - 1, col + 1, playingBoard) == false)
+						playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
+							boardSize, row - 1, col + 1, movesLeft, command);
+				}
+
+				//right 'neighbour' - 3 o'clock
+				if (isValid(row, col + 1, boardSize) == true)
+				{
+					if (isMine(row, col + 1, playingBoard) == false)
+						playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
+							boardSize, row, col + 1, movesLeft, command);
+				}
+
+				//diagonal down-right 'neighbour' - 4:30 o'clock
+				if (isValid(row + 1, col + 1, boardSize) == true)
+				{
+					if (isMine(row + 1, col + 1, playingBoard) == false)
+						playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
+							boardSize, row + 1, col + 1, movesLeft, command);
+				}
+
+				//down 'neighbour' - 6 o'clock
+				if (isValid(row + 1, col, boardSize) == true)
+				{
+					if (isMine(row + 1, col, playingBoard) == false)
+						playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
+							boardSize, row + 1, col, movesLeft, command);
+				}
+
+				//diagonal down-left 'neighbour' - 7:30 o'clock
+				if (isValid(row + 1, col - 1, boardSize) == true)
+				{
+					if (isMine(row + 1, col - 1, playingBoard) == false)
+						playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
+							boardSize, row + 1, col - 1, movesLeft, command);
+				}
+
+				//left 'neighbour' - 9 o'clock
+				if (isValid(row, col - 1, boardSize) == true)
+				{
+					if (isMine(row, col - 1, playingBoard) == false)
+						playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
+							boardSize, row, col - 1, movesLeft, command);
+				}
+
+				//diagonal up-left 'neighbour' - 7:30 o'clock
+				if (isValid(row - 1, col - 1, boardSize) == true)
+				{
+					if (isMine(row - 1, col - 1, playingBoard) == false)
+						playMinesweeperUtil(myBoard, playingBoard, mines, amountOfMines,
+							boardSize, row - 1, col - 1, movesLeft, command);
+				}
+			}
+
+			return (false);
+		}
+	}
+}
+
+// A function to replace the mine from (row, col) and put an empty cell
+// It is in order to make the first move safe
+void replaceMine(int row, int col, char board[][MAX_SIZE], int boardSize)
+{
+	for (int i = 0; i < boardSize; i++)
+	{
+		for (int j = 0; j < boardSize; j++)
+		{
+			// Find the first location in the board
+			// which is not having a mine and put a mine
+			// there.
+			if (board[i][j] != '*')
+			{
+				board[i][j] = '*';
+				board[row][col] = '-';
+				return;
+			}
+		}
+	}
+	return;
+}
+
+// A Function to play Minesweeper game
+void playMinesweeper(int boardSize, int amountOfMines)
+{
+	// Initially the game is not over
+	bool gameOver = false;
+
+	// Actual Board and My Board
+	char realBoard[MAX_SIZE][MAX_SIZE], myBoard[MAX_SIZE][MAX_SIZE];
+
+	int movesLeft = boardSize * boardSize - amountOfMines;
+	int x{}, y{};
+	int mines[MAX_MINES][2]; // stores (x,y) coordinates of all mines.
+
+	CreateBoardBeforeStart(realBoard, myBoard, boardSize);
+
+	// Place the Mines randomly
+	placeMines(mines, amountOfMines, realBoard, boardSize);
+
+	// You are in the game until you have not opened a mine
+
+	int currentMoveIndex = 0;
+
+	while (gameOver == false)
+	{
+		printf("Current Status of Board : \n");
+		printBoard(myBoard,  boardSize);
+
+		char* command = makeMove(x, y);
+
+		// This is to guarantee that the first move is
+		// always safe
+		if (currentMoveIndex == 0)
+		{
+			// If the first move is a mine
+			// then we remove the mine from that location
+			if (isMine(x, y, realBoard) == true)
+				replaceMine(x, y, realBoard, boardSize);
+		}
+
+		currentMoveIndex++;
+
+		gameOver = playMinesweeperUtil(myBoard, realBoard, mines, amountOfMines,
+			boardSize, x, y, &movesLeft, command);
+
+		if ((gameOver == false) && (movesLeft == 0))
+		{
+			printf("\nYou won !\n");
+			gameOver = true;
+		}
+	}
+	return;
 }
 
 int main()
@@ -345,13 +480,7 @@ int main()
 
 	InputValidation(boardSize, amountOfMines);
 
-	char myBoard[MAX_SIZE][MAX_SIZE]{};
-
-	char playingBoard[MAX_SIZE][MAX_SIZE]{};
-
-	CreateBoardBeforeStart(myBoard, playingBoard, boardSize);
-
-	printBoard(myBoard, boardSize);
+	playMinesweeper(boardSize, amountOfMines);
 
 	return 0;
 }
